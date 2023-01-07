5d4668448854e886ce2eade8f614f3d599889a17
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-01-07 20:50:07 +0900

    add nixpkgs-{unstable,22_05,21_11,21_05}

--- flake.nix
+++ flake.nix
@@ -1,7 +1,12 @@
 {
   inputs = {
 
+    nixpkgs-unstable.url = github:nixos/nixpkgs/nixos-unstable;
+
     nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+    nixpkgs-22_05.url = github:nixos/nixpkgs/nixos-22.05;
+    nixpkgs-21_11.url = github:nixos/nixpkgs/nixos-21.11;
+    nixpkgs-21_05.url = github:nixos/nixpkgs/nixos-21.05;
 
     flake-utils.url = github:numtide/flake-utils/master;
 
